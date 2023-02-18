3e555a784e0c50f162c3387439429d5be681dc19
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-02-18 20:58:35 +0100

    updated

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,10 +1,10 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
-router: {
-    base: '/KaanaServices/'
-  },  
-docs: {
-    primaryColor: '#E24F55'
-  }
-})
+  router: {
+    base: "/KaanaServices/",
+  },
+  docs: {
+    primaryColor: "#E24F55",
+  },
+});
