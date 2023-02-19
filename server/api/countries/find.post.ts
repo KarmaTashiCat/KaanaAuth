7d19ff4a5c82ed905e5838ace7b703e90d71736e
Albert Isern Alvarez <albert.isern.alvarez@gmail.com>
2023-02-19 16:07:35 +0100

    Added API

--- server/api/countries/find.post.ts
+++
@@ -1,9 +0,0 @@
-import { countryList } from "@doubco/countries";
-
-export default eventHandler(async (event) => {
-  const body = await readBody(event);
-  const { country } = body;
-  return {
-    countries: countryList,
-  };
-});
